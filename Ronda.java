da72ead594f421642a3a28c956e681b657b0d642
Metalderly2023 <129208394+Metalderly2023@users.noreply.github.com>
2023-04-09 10:36:04 -0300

    subuendo clase y persona

---
+++ Ronda.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Ronda {
+    private String numero;
+    private ArrayList<Partido> partidos = new ArrayList();
+
+    public Ronda(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return this.partidos;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+}
