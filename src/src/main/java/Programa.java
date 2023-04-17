8976cac5db4ac2c4899e3048d0a16ac8f47c4efe
Metalderly2023 <osvaldoeverdiaz@gmail.com>
2023-04-17 18:33:28 -0300

    ya se conecta a la base y reparando otros errores

--- src/src/main/java/Programa.java
+++ src/src/main/java/Programa.java
@@ -8,17 +8,9 @@ import java.util.ArrayList;
 
 public class Programa {
     public static void main(String[] args) throws IOException, SQLException {
-        /*
-        Path archivo1=Paths.get("Resultados.csv");
-        Path archivo2=Paths.get("Apuestas.csv");
 
-        Scanner lector1 = new Scanner(archivo1);
-        Scanner lector2 = new Scanner(archivo2);
-        lector1.useDelimiter("[;]");
-        lector2.useDelimiter("[;]");
-        */
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql-server://localhost/Prode_BD", "deo", "29419977");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/Prode_BD", "root", "Oedsoltero*/40");
 
         Statement st = connection.createStatement();
         ResultSet Ap = st.executeQuery("SELECT * FROM Apuestas");
@@ -29,53 +21,26 @@ public class Programa {
         ArrayList<Pronostico> pronosticos = new ArrayList<Pronostico>();
 
 
-        Statement stt = connection.createStatement();
-        ResultSet ronda = stt.executeQuery("SELECT * FROM Resultados");
-        //Ronda ronda = new Ronda (ron);
+
+        Ronda ronda = new Ronda (1);
 
         while (Rs.next()) {
             String e1 = Rs.getString("Equipo1");
-            int goles1 = Rs.getInt("Goles");
+            int goles1 = Rs.getInt("Goles1");
             String e2 = Rs.getString("Equipo2");
             int goles2 = Rs.getInt("Goles2");
 
-        //Statement stt = connection.createStatement();
-        //ResultSet ronda = stt.executeQuery("SELECT * FROM Resultados");
-        //Ronda ronda = new Ronda (ron);
-        /*
 
-        while (lector1.hasNext()) {
-
-            String e1=lector1.next();
-            int goles1= lector1.nextInt();
-            String e2=lector1.next();
-            int goles2= lector1.nextInt();
-        */
-            Equipo equipo1 = new Equipo(Rs.getString("Equiupo1"));
+            Equipo equipo1 = new Equipo(Rs.getString("Equipo1"));
             Equipo equipo2 = new Equipo(Rs.getString("Equipo2"));
 
             Partido partido1 = new Partido(equipo1, equipo2, goles1, goles2);
 
             partidos.add(partido1);
 
-            ronda.getWarnings();
+            ronda.setPartidos(partidos);
         }
 
-        Rs.close();
-        /*
-        lector1.close();
-        while (lector2.hasNext()) {
-            String nom=lector2.next();
-            String ape=lector2.next();
-            int dni=lector2.nextInt();
-            String e1=lector2.next();
-            int goles1=lector2.nextInt();
-            String e2=lector2.next();
-            int goles2=lector2.nextInt();
-        */
-
-        //Statement stet = connection.createStatement();
-        //ResultSet rondas = stet.executeQuery("SELECT * FROM Apuestas");
         while (Ap.next()) {
             String nom = Ap.getString("Nombre");
             String ape = Ap.getString("Apellido");
@@ -98,15 +63,12 @@ public class Programa {
 
             ronda.setPronosticos(pronosticos);
         }
+        Rs.close();
         Ap.close();
 
         connection.close();
 
 
-        /*ector2.close();
-
-        ronda.aciertoPorRonda();
-        */
     }
 
 
